b18b52c04cc8a3d8c8d06b1ab212ba37d8faa638
gyungchan Jo <namejgc@naver.com>
2023-05-22 11:50:10 +0900

    백준 - 구현 알고리즘 문제 풀이

---
+++ src/baekjoon/implementation/ACM호텔.java
@@ -0,0 +1,25 @@
+package baekjoon.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ACM호텔 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int H = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+
+            int distance = (N - 1) / H + 1;
+            int floor = (N - 1) % H + 1;
+            System.out.printf("%d%02d\n", floor, distance);
+        }
+    }
+}
