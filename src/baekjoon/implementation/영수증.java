115c01a0cf49d522ab94bd1611b6ec1e6a88a881
gyungchan Jo <namejgc@naver.com>
2023-06-04 01:48:47 +0900

    백준 - 구현 알고리즘 문제 풀이

---
+++ src/baekjoon/implementation/영수증.java
@@ -0,0 +1,30 @@
+package baekjoon.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 영수증 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        int sum = 0;
+        while (N-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sum += a * b;
+        }
+
+        if (sum == X) {
+            System.out.println("Yes");
+            return;
+        }
+        System.out.println("No");
+    }
+}
