3d0382addb0b98fc16c9527ef82501905d2332ba
gyungchan Jo <namejgc@naver.com>
2023-07-17 19:45:58 +0900

    프로그래머스 - lv2 알고리즘 문제 풀이

---
+++ src/programmers/lv2/행렬의곱셈.java
@@ -0,0 +1,29 @@
+package programmers.lv2;
+
+public class 행렬의곱셈 {
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr1[0].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr1 = {{2, 3, 2}, {4, 2, 4}, {3, 1, 4}};
+        int[][] arr2 = {{5, 4, 3}, {2, 4, 1}, {3, 1, 1}};
+
+        for (int[] answers : solution(arr1, arr2)) {
+            for (int answer : answers) {
+                System.out.print(answer + " ");
+            }
+            System.out.println();
+        }
+    }
+}
